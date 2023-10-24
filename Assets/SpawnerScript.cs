5d4c4adf3bd67700c77b1cc7868cf98e0928bb1b
samirN99 <samirnaran21@gmail.com>
2023-10-24 16:53:30 +0100

    fixed object spawn againn
    objects werents solid and player was going through the objects
    FIXED

--- Assets/SpawnerScript.cs
+++ Assets/SpawnerScript.cs
@@ -32,7 +32,7 @@ public class SpawnerScript : MonoBehaviour
     private void Spawn()
     {
         GameObject obstacleToSpawn = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
-        GameObject spawnedObstacle = Instantiate(obstacleToSpawn, transform.position, Quaternion.identity);
+        GameObject spawnedObstacle = Instantiate(obstacleToSpawn, transform.position, Quaternion.identity);  //will spawn in at the rotation the prefab is set to. Quaternion.identity means no rotation
 
         Rigidbody2D obstacleRb = spawnedObstacle.GetComponent<Rigidbody2D>();
         obstacleRb.velocity = Vector2.left * obstacleSpeed;
