d8621c5e6f28f6e9818e0ca5aeaba61630c1a64d
samirN99 <samirnaran21@gmail.com>
2023-10-24 15:31:26 +0100

    added the player obstacles main spawn code

---
+++ Assets/SpawnerScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class SpawnerScript : MonoBehaviour
+{
+    [SerializeField] private GameObject[] obstaclePrefabs;
+    public float obstacleSpawnTime = 2f;
+    public float obstacleSpeed = 1f;
+
+    private float timeUntilObstacleSpawn;
+    
+    private void Update()
+    {
+        SpawnLoop();
+    }
+
+    private void SpawnLoop()
+    {
+        timeUntilObstacleSpawn += Time.deltaTime;
+
+        if (timeUntilObstacleSpawn >= obstacleSpawnTime)
+        {
+            Spawn();
+            timeUntilObstacleSpawn = 0f;
+        }
+
+        
+    }
+
+    private void Spawn()
+    {
+        GameObject obstacleToSpawn = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+        GameObject spawnedObstacle = Instantiate(obstacleToSpawn, transform.position, Quaternion.identity);
+
+        Rigidbody2D obstacleRb = spawnedObstacle.GetComponent<Rigidbody2D>();
+        obstacleRb.velocity = Vector2.left * obstacleSpeed;
+    }
+
+
+}
